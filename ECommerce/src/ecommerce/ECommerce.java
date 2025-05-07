4f2ebc8caad5f2aeae17ad3159ab53de0cf6babe
wadiijbili <jbili.wadii.21@itisriva.edu.it>
2025-05-07 11:59:39 +0200

    fatto parte 2 e 4
    ss

--- ECommerce/src/ecommerce/ECommerce.java
+++ ECommerce/src/ecommerce/ECommerce.java
@@ -17,68 +17,100 @@ public class ECommerce {
     /**
      * @param args the command line arguments
      */
-    
     public static void main(String[] args) {
         // TODO code application logic here
-        ArrayList <Prodotto> carrello = new ArrayList();
-        ArrayList <Prodotto> prodotti = new ArrayList();
-        Prodotto DVD = new Prodotto("dvd","eletronica",15);
-        Prodotto CELLULARE = new Prodotto("cellulare","eletronica",100);
-        Prodotto MAGLIETTA = new Prodotto("maglietta","indumenti",5);
-        Prodotto SCARPE = new Prodotto("scarpe","indumenti",50);
-        
-        prodotti.add(DVD);
-        prodotti.add(SCARPE);
-        prodotti.add(CELLULARE);
-        prodotti.add(MAGLIETTA);
-        
+        ArrayList<Prodotto> carrello = new ArrayList();
+        ArrayList<Prodotto> prodotti = new ArrayList();
+        Prodotto dvd = new Prodotto("dvd", "eletronica", 15);
+        Prodotto cellulare = new Prodotto("cellulare", "eletronica", 100);
+        Prodotto maglietta = new Prodotto("maglietta", "indumenti", 5);
+        Prodotto scarpe = new Prodotto("scarpe", "indumenti", 50);
+
+        prodotti.add(dvd);
+        prodotti.add(scarpe);
+        prodotti.add(cellulare);
+        prodotti.add(maglietta);
+        carrello.add(dvd);
+
         Scanner scelta = new Scanner(System.in);
-        
+        int h = 0;
+       
+        while(h == 0){
+             menu();
+        switch (scelta.nextLine()) {
+            case "1":
+                /*   System.out.println(DVD.getNome()+" "+ DVD.getCategoria()+" "+ DVD.getPrezzo()+"€");
+                System.out.println(CELLULARE.getNome()+" "+ CELLULARE.getCategoria()+" "+ CELLULARE.getPrezzo()+"€");
+                System.out.println(MAGLIETTA.getNome()+" "+ MAGLIETTA.getCategoria()+" "+ MAGLIETTA.getPrezzo()+"€");
+                System.out.println(SCARPE.getNome()+" "+ SCARPE.getCategoria()+" "+ SCARPE.getPrezzo()+"€");
+                 */
+                for (int i = 0; i < prodotti.size(); i++) {
+                    System.out.print(prodotti.get(i).getNome() + " ");
+                    System.out.print(prodotti.get(i).getCategoria() + " ");
+                    System.out.print(prodotti.get(i).getPrezzo() + "€");
+                    System.out.println();
+                }
+                break;
+            case "2":
+                int c = 0;
+                while (c == 0) {
+
+                    System.out.println("quale prodotto desideri aggiungere?");
+
+                    for (int i = 0; i < 4; i++) {
+                        System.out.print(prodotti.get(i).getNome() + " ");
+                        System.out.print(prodotti.get(i).getCategoria() + " ");
+                        System.out.print(prodotti.get(i).getPrezzo() + "€");
+                        System.out.println();
+
+                    }
+                    if (c == 0) {
+                        String input = scelta.nextLine();
+                        for (Prodotto i : prodotti) {
+                            if (i.getNome() == input) {
+                                carrello.add(i);
+                            }
+
+                        }
+
+                    }
+                    System.out.println("vuoi acquistare altro? si o no");
+                    String replay = scelta.nextLine();
+                    if (replay == "si") {
+                        c = 0;
+                    } else {
+                        c = 1;
+                    }
+
+                }
+                if (c == 1) {
+                    break;
+                }
+
+            case "3":
+
+                break;
+            case "4":
+                for (int i = 0; i < carrello.size(); i++) {
+                    System.out.print(carrello.get(i).getNome() + " ");
+                    System.out.print(carrello.get(i).getCategoria() + " ");
+                    System.out.print(carrello.get(i).getPrezzo() + "€");
+                    System.out.println();
+                }
+                break;
+            case "5":
+                return;
+        }
+        }
+
+    }
+
+    public static void menu() {
+
         System.out.println("------MENU------");
         System.out.println("1. VISUALIZZA PRODOTTI");
         System.out.println("2. AGGIUNGI PRODOTTI AL CARELLO");
         System.out.println("3. RICERCA PRODOTTI PER CATEGORIA");
         System.out.println("4. STAMPA CONTENUTO CARRELLO");
-    
-        switch(scelta.nextLine()){
-            case "1":
-             /*   System.out.println(DVD.getNome()+" "+ DVD.getCategoria()+" "+ DVD.getPrezzo()+"€");
-                System.out.println(CELLULARE.getNome()+" "+ CELLULARE.getCategoria()+" "+ CELLULARE.getPrezzo()+"€");
-                System.out.println(MAGLIETTA.getNome()+" "+ MAGLIETTA.getCategoria()+" "+ MAGLIETTA.getPrezzo()+"€");
-                System.out.println(SCARPE.getNome()+" "+ SCARPE.getCategoria()+" "+ SCARPE.getPrezzo()+"€");
-            */
-                for(int i=0; i < prodotti.size(); i++){
-                System.out.print(prodotti.get(i).getNome()+" ");
-                System.out.print(prodotti.get(i).getCategoria()+" ");
-                System.out.print(prodotti.get(i).getPrezzo()+"€");
-                System.out.println();
-                }
-                break;
-            case "2":
-                for(int c = 0;c != 0;){
-                    
-                System.out.println("quale prodotto desideri aggiungere?");
-                
-                for(int i=0; i < prodotti.size(); i++){
-                System.out.print(prodotti.get(i).getNome()+" ");
-                System.out.print(prodotti.get(i).getCategoria()+" ");
-                System.out.print(prodotti.get(i).getPrezzo()+"€");
-                System.out.println();
-                }
-                
-                carrello.add();
-                
-                
-                }
-        }
-        
-        
-
-                
     }
-
-    
-    
-    
-    
 }
